16f1775641a5a9d276a23f96d4ebd69fc393ddc1
rajesh mishra <rajeshmishra847410@gmail.com>
2025-01-25 17:37:22 +0530

    updated database configuration

--- db_config.py
+++ db_config.py
@@ -2,11 +2,10 @@ import pymysql
 
 def get_db_connection():
     return pymysql.connect(
-        host="localhost",
-        user="root",  # my MySQL username
-        password="rajeshji11",  # my MySQL password
-        database="expense_tracker"
+        host="dpg-cuacr73qf0us73c869g0-a",
+        user="root",
+        password="u3Q6m50wS9TcrxI4N8GjGIlTccHH83jI",
+        database="expense_tracker_g6lz",
+        port=5432,
+        charset="utf8mb4",
     )
-
-
-
