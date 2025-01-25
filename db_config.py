744c7e85d6f822e6a94e58eb4582c6ba91163146
rajesh mishra <rajeshmishra847410@gmail.com>
2025-01-25 18:09:30 +0530

    updated database configuration

--- db_config.py
+++ db_config.py
@@ -1,11 +1,16 @@
 import pymysql
+import os
 
 def get_db_connection():
     return pymysql.connect(
-        host="dpg-cuacr73qf0us73c869g0-a",
-        user="root",
-        password="u3Q6m50wS9TcrxI4N8GjGIlTccHH83jI",
-        database="expense_tracker_g6lz",
-        port=5432,
+        host=os.getenv("dpg-cuacr73qf0us73c869g0-a"),
+        user=os.getenv("root"),
+        password=os.getenv("u3Q6m50wS9TcrxI4N8GjGIlTccHH83jI"),
+        database=os.getenv("expense_tracker_g6lz"),
+        port=int(os.getenv("5432")),
         charset="utf8mb4",
     )
+
+
+
+
